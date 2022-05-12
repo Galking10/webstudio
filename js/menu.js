cea3590ad359357b39e49192d4f7bdf9fde149cf
Galking10 <krllglkn1807@gmail.com>
2022-05-12 18:40:44 +0200

    test3

--- js/menu.js
+++ js/menu.js
@@ -1,12 +1,14 @@
 (() => {
     const menuBtnRef = document.querySelector("[data-menu-btn]");
     const mobileMenuRef = document.querySelector("[data-menu]");
+    const mobileMenuLogo = document.querySelector("[data-logo]");
   
     menuBtnRef.addEventListener("click", () => {
       const expanded =
         menuBtnRef.getAttribute("aria-expanded") === "true" || false;
   
       menuBtnRef.classList.toggle("is-open");
+      mobileMenuLogo.classList.toggle("is-open");
       menuBtnRef.setAttribute("aria-expanded", !expanded);
   
       mobileMenuRef.classList.toggle("is-open");
